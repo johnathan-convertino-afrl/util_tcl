b4752a60dee15786a0211078ab2a0ce5b01ffb84
Jay Convertino <johnathan.convertino.1@us.af.mil>
2023-12-14 19:11:09 -0500

    update names to remove vivado prefix. future tools will swap out scripts with same names and similar functionality based on the tool.

--- helper_check/src/helper_vivado.tcl
+++ helper_check/src/helper_vivado.tcl
@@ -1,5 +1,5 @@
 #check hierarchy state of the software
-proc vivado_hierarchy_check {requested_mode} {
+proc hierarchy_check {requested_mode} {
   set hierarchy_mode [get_property STEPS.SYNTH_DESIGN.ARGS.FLATTEN_HIERARCHY [get_runs synth_1]]
 
   if {[string compare $hierarchy_mode $requested_mode] != 0} {
@@ -11,7 +11,7 @@ proc vivado_hierarchy_check {requested_mode} {
 }
 
 #check ip version by susing vlvn
-proc vivado_ip_vlvn_version_check {req_ip_version} {
+proc ip_vlvn_version_check {req_ip_version} {
   set ip_version [get_ipdefs -quiet -all -vlnv $req_ip_version]
 
   if {$ip_version eq ""} {
@@ -22,7 +22,7 @@ proc vivado_ip_vlvn_version_check {req_ip_version} {
   }
 }
 
-proc vivado_version_check {req_vivado_version} {
+proc version_check {req_vivado_version} {
   set vivado_version [version -short]
 
   if {[string compare $vivado_version $req_vivado_version] != 0} {
